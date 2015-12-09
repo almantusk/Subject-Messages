34668b820dcfadab3344cfe14db0480bb9aee8a1
almantusk <almantas.karpavicius@ktu.edu>
2015-12-09 22:39:27 +0200

    Pilnas index.php failas projektui

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
-<?php
+﻿<?php
+header('Content-type: text/html; charset=UTF-8') ;
 define('hostname', 'localhost');
 define('user','u560012736_stud');
 define('password', '163630');
 define('databaseName', 'u560012736_mess');
 $connect = mysqli_connect(hostname, user, password, databaseName);
-
 if($_SERVER["REQUEST_METHOD"]=="POST")
 {
     createMessage();
 }
 if($_SERVER["REQUEST_METHOD"]=="GET")
 {
-    getMessage();
+    //getMessage();
 }
 
 function createMessage()
@@ -21,7 +21,7 @@ $sql;
 global $connect;
 $data = json_decode(file_get_contents('php://input'), true);
 $i = count($data);
-if($i ==1)
+if($i == 1)
 {
   if($data["all"])
   $sql = "SELECT * FROM `messages`";
@@ -31,10 +31,10 @@ if($i ==1)
    $sql = "SELECT * FROM `messages`WHERE message_id =".$data["message_id"];
   else if($data["module_code"])
    $sql = "SELECT * FROM `messages`WHERE module_code =\"".$data["module_code"]. "\"";
-   else if($data["module_name"])
-   $sql = "SELECT * FROM `messages`WHERE module_name  =\"".$data["module_name"]. "\"";
-   else if($data["message_date"])
+  else if($data["message_date"])
    $sql = "SELECT * FROM `messages`WHERE message_date  =\"".$data["message_date"]. "\"";
+  else if($data["lecture_type"])
+   $sql = "SELECT * FROM `messages`WHERE lecture_type  =\"".$data["lecture_type"]. "\"";
     $result = $connect->query($sql);
     if ($result->num_rows > 0)
     {
@@ -42,28 +42,108 @@ if($i ==1)
      while($r = mysqli_fetch_assoc($result)) {
      $rows[] = $r;
     }
-    print json_encode($rows);
+	header('content-type: application/json; charset=utf-8');
+    echo json_encode($rows, JSON_UNESCAPED_UNICODE);
     } 
     else 
     {
-      echo "0 results";
+      $rows = array();
+      //echo "0 results";
+      header('content-type: application/json; charset=utf-8');
+      echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+    }
+}
+else if ($i == 2)
+{
+	if($data["lecture_type"])
+	{
+		if($data["user_id"])
+			$sql = "SELECT * FROM `messages`WHERE user_id  =\"".$data["user_id"]. "\" ";
+		else if($data["message_id"])
+			$sql = "SELECT * FROM `messages`WHERE message_id =".$data["message_id"];
+		else if($data["module_code"])
+			$sql = "SELECT * FROM `messages`WHERE module_code =\"".$data["module_code"]. "\" ";
+		else if($data["message_date"])
+			$sql = "SELECT * FROM `messages`WHERE message_date  =\"".$data["message_date"]. "\" ";
+		$sql = $sql."AND lecture_type =\"".$data["lecture_type"]. "\"";
+	}
+	$result = $connect->query($sql);
+    if ($result->num_rows > 0)
+    {
+     $rows = array();
+     while($r = mysqli_fetch_assoc($result)) {
+     $rows[] = $r;
+    }
+	header('content-type: application/json; charset=utf-8');
+    echo json_encode($rows, JSON_UNESCAPED_UNICODE);
+    } 
+    else 
+    {
+      $rows = array();
+      //echo "0 results";
+      header('content-type: application/json; charset=utf-8');
+      echo json_encode($rows, JSON_UNESCAPED_UNICODE);
     }
 }
 else
 {
-    $user_id = $data["user_id"];
-    $user_name = $data["user_name"];
-    $message_content = $data["message_content"];
-    $message_attach = $data["message_attach"];
-    $message_date = $data["message_date"];
-    $module_code = $data["module_code"];
-    $module_name = $data["module_name"];
-    $query = "Insert into messages(user_id, user_name, message_content, message_attach, message_date, module_code, module_name) 
-    values ('$user_id','$user_name', '$message_content', '$message_attach', '$message_date', '$module_code', '$module_name')";
+	$counter = 0;
+	if(!$data["user_id"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": user id missing..Default user_id value: \"anonym\".\n";
+		$user_id = "anonym";
+	}
+	else
+		$user_id = $data["user_id"];
+	if(!$data["user_name"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": user name missing..Default user_name value: \"anonymous\".\n";
+		$user_name = "anonymous";
+	}
+	else
+		$user_name = $data["user_name"];
+	if(!$data["message_content"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": message content missing..Default message_content value: \"-\".\n";
+		$message_content = "-";
+	}
+	else
+		$message_content = $data["message_content"];
+	if(!$data["message_attach"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": message attach missing..Default message_attach value: \"-\".\n";
+		$message_attach = "-";
+	}
+	else
+		$message_attach = $data["message_attach"];
+	if(!$data["module_code"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": module code missing..Default module_code value: \"P00000\".\n";
+		$module_code = "P00000";
+	}
+	else
+		$module_code = $data["module_code"];
+	if(!$data["lecture_type"])
+	{
+		$counter = $counter + 1;
+		echo "Warning".$counter.": lecture type missing..Default lecture_type value: \"all\".\n";
+		$lecture_type = "general";
+	}
+	else
+		$lecture_type = $data["lecture_type"];
+    $query = "Insert into messages(user_id, user_name, message_content, message_attach, module_code, lecture_type) 
+    values ('$user_id','$user_name', '$message_content', '$message_attach', '$module_code', '$lecture_type')";
     mysqli_query($connect, $query) or die (mysqli_error($connect));
     mysqli_close($connect);
+    echo "\nThe json has been inserted to the \"messages\" databse.\n";
 }
 }
+
 
 function getMessage()
 {
@@ -93,8 +173,7 @@ function getMessage()
       {
         echo "user_id: ".$row["user_id"]. 
         " user_name: " . $row["user_name"]. 
-        " message_content: ". $row["message_content"] .         
-        " module_name: ". $row["module_name"];
+        " message_content: ". $row["message_content"];
       }
     } 
     else 
@@ -103,4 +182,4 @@ function getMessage()
     }
   $connect->close();
 }
-?>
+?>	
